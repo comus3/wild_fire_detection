0b6de63a5bbad4eb2c0cfe1702ee65ffbc764014
comus3 <come.plantin.carrenard@gmail.com>
2024-10-21 16:32:57 +0200

    working communication with device

--- src/server.js
+++ src/server.js
@@ -18,16 +18,32 @@ const mqttOptions = {
     password: process.env.API_KEY_MQTT,
 };
 
+console.log('Connecting to MQTT broker at:', process.env.MQTT_BROKER_URL);
+console.log('Using MQTT options:', mqttOptions);
+
+
 // Connect to TTN and subscribe to your device
 const client = mqtt.connect(process.env.MQTT_BROKER_URL, mqttOptions);
 
 client.on('connect', () => {
-    client.subscribe('your-sde');
+    console.log('Connected to MQTT broker');
+    client.subscribe([`v3/${process.env.USER_NAME_MQTT}/devices/+/up`], (err) => {
+        if (err) {
+            console.error('Subscription error:', err);
+        } else {
+            console.log('Subscribed to topic: wfd-end');
+        }
+    });
 });
 
-// When TTN sends a message, notify the client via socket.io
+
 client.on('message', (topic, message) => {
-    io.emit('event-name', message.toString()); // Convert message to string if needed
+    console.log(`Received message from ${topic}: ${message.toString()}`);
+    io.emit('event-name', message.toString());
+});
+
+client.on('error', (err) => {
+    console.error('Connection error:', err);
 });
 
 // Start the server
